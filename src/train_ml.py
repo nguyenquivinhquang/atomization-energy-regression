95a9605e84a14fd11bc22eee17ffe90d12699374
Nguyễn Quí Vinh Quang <60070577+nguyenquivinhquang@users.noreply.github.com>
2023-06-19 00:05:27 +0700

    i

--- src/train_ml.py
+++ src/train_ml.py
@@ -4,18 +4,19 @@ from src.data_processing import make_data_loader_ml
 from src.model import build_regression_model
 from processor.trainer.ml_trainer import MLTrain
 from sklearn.metrics import mean_absolute_error
-
+import time
 
 # from src.
 def train(cfg):
     print(cfg)
     train_loader, val_loader, scale = make_data_loader_ml(cfg)
     model = build_regression_model(cfg)
-
+    start_time = time.time()
     trainer = MLTrain(
         model, train_loader, val_loader, mean_absolute_error, cfg, scale=scale
     )
     trainer.process()
+    print("Training time: {}".format(time.time() - start_time))
     return
 
 
