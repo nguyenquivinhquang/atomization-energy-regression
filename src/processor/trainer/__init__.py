ff7cc328020dac9968b1af7ffa9b8d2c972a056b
Nguyễn Quí Vinh Quang <60070577+nguyenquivinhquang@users.noreply.github.com>
2023-06-20 00:45:35 +0700

    Update __init__.py

--- src/processor/trainer/__init__.py
+++ src/processor/trainer/__init__.py
@@ -4,9 +4,9 @@ def build_trainer(cfg, model, train_loader, val_loader, optimizer, scheduler, lo
     if cfg['MODEL']['MODEL_NAME'] == 'MLP':
         trainer = Trainer(model, train_loader, val_loader, optimizer, scheduler, loss_fn, total_epochs)
     elif cfg['MODEL']['MODEL_NAME'] == 'GAT':
-        trainer = DeepTrainer(model, train_loader, val_loader, optimizer, scheduler, loss_fn, total_epochs)
+        trainer = GraphTrainer(model, train_loader, val_loader, optimizer, scheduler, loss_fn, total_epochs)
     elif cfg['MODEL']['MODEL_NAME'] == 'GCN':
-        trainer = DeepTrainer(model, train_loader, val_loader, optimizer, scheduler, loss_fn, total_epochs)
+        trainer = GraphTrainer(model, train_loader, val_loader, optimizer, scheduler, loss_fn, total_epochs)
     else:
         trainer = Trainer(model, train_loader, val_loader, optimizer, scheduler, loss_fn, total_epochs)
     return trainer
